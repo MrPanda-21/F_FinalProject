a0973498cefcda9f10cbd4789ecfe8244b9e0e46
MrPand <ahmet.arda.acar@hkal.k12.tr>
2021-02-16 00:29:13 +0300

    Birthday of WebApi
    Today is the birthday of webapi layer.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using FinalBusiness.Abstract;
+using FinalBusiness.Concrete;
+using FinalDataAccess.Concrete.EntityFreamwork;
+using FinalEntities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    { 
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            IProductService productService =
+                new ProductManager(new EfProductDal());
+            var result = productService.GetAll();
+            return result.Data;
+            //return new List<Product>
+            //{
+            //    new Product{CategoryId = 1, ProductName = "Apple"},
+            //    new Product{CategoryId = 2, ProductName = "Peer"},
+            //};
+        }
+    }
+}
